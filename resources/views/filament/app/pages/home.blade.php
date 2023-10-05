58e018f2288fb4451b896f0eacc787905899f7e5
Lalhmangaihenga <valpuia604@gmail.com>
2023-10-05 09:55:55 +0700

    update info in home

--- resources/views/filament/app/pages/home.blade.php
+++ resources/views/filament/app/pages/home.blade.php
@@ -1,8 +1,10 @@
 <x-filament-panels::page>
 
-    AppPanelProvider is used as landing page (this page) <br>
+    "\App\Providers\Filament\AppPanelProvider" is used for the layouts <br><br>
+
+    "\App\Filament\App\Pages\Home" is used for this landing page <br><br>
 
     Right click on Dashboard from navbar and open
-    on new tab then switch theme, <a href="/" target="_blank" class="underline">or click here</a>
+    on new tab then switch to any theme <a href="/" target="_blank" class="underline">or click here and switch theme</a>
 
 </x-filament-panels::page>
