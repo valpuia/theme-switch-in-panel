c1e53d3982a6429b192d81406130af3953806766
Lalhmangaihenga <valpuia604@gmail.com>
2023-10-05 09:42:50 +0700

    initial

---
+++ app/Filament/App/Pages/Home.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\App\Pages;
+
+class Home extends \Filament\Pages\Dashboard
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.app.pages.home';
+}
---
+++ resources/views/filament/app/pages/home.blade.php
@@ -0,0 +1,8 @@
+<x-filament-panels::page>
+
+    AppPanelProvider is used as landing page (this page) <br>
+
+    Right click on Dashboard from navbar and open
+    on new tab then switch theme, <a href="/" target="_blank" class="underline">or click here</a>
+
+</x-filament-panels::page>
